9f95c48fe8c35c7f8a0d6ba9da7f6291b3f495bd
drong24 <daisyrong24@gmail.com>
2023-03-24 23:22:28 +0900

    added date to journal entries

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ function getRs() {
     let txt = getTxt('txtIn').value
     const date = new Date()
 
-    getTxt('entry').innerHTML = `<div>${txtIn.value}</div>`
+    getTxt('entry').innerHTML += `<div><p>${txtIn.value}</p>
+    <small>${date.toLocaleDateString()}</small?</div>`
 }
