735cbcda84adfd137cdab2ecebcac3d0c3d8552d
drong24 <daisyrong24@gmail.com>
2023-03-24 23:33:07 +0900

    added time in entry txtIn is removed after saving text

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function getRs() {
     let txt = getTxt('txtIn').value
     const date = new Date()
 
-    getTxt('entry').innerHTML += `<div><p>${txtIn.value}</p>
-    <small>${date.toLocaleDateString()}</small?</div>`
+    getTxt('entry').innerHTML += `<div class="card"><p>${txtIn.value}</p>
+    <small>${date.toLocaleTimeString()}, 
+    ${date.toLocaleDateString()}</small?</div>`
+    document.getElementById('txtIn').remove()
+
 }
