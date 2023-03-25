a7610e2a6a71eea05abfdc6ad60c853e2c772f61
drong24 <daisyrong24@gmail.com>
2023-03-25 21:27:06 +0900

    add entry button in journal is shown on toggle, text in entry box is cleared after save button is pressed

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+
 function getTxt(id) {
     return document.getElementById(id)
 }
 
 function getRs() {
-    let txt = getTxt('txtIn').value
+    var txt = getTxt('txtIn')
     const date = new Date()
 
     getTxt('entry').innerHTML += `<div class="card"><p>${txtIn.value}</p>
     <small class="dateTime">${date.toLocaleTimeString()}, 
     ${date.toLocaleDateString()}</small?</div>`
-    document.getElementById('txtIn').remove()
-    document.getElementById('saveButton').remove()
+    txt.value = '';
+    addText()
 
 }
 
 function addText() {
-    getTxt('enter').innerHTML = `<div><textarea class="journal_entry" id="txtIn"></textarea>
-    <button onclick ="getRs()" class="journal_button" id="saveButton" type="submit">Save</button></div>`
+    var inputBox = getTxt('enter')
+    if (inputBox.style.display == "none") {
+
+        inputBox.style.display = 'block';
+    }
+    else {
+        inputBox.style.display = "none";
+    }
+
 }
